fad74e7e48e9a695b30d56fe6c944e14ed9b0111
freshprinceofcoders <milanosolanki27@gmail.com>
2020-06-10 19:55:21 +0100

    Dice to random randint

--- dice-game.py
+++ dice-game.py
@@ -1,1 +1,22 @@
+import random
+dice = random.randint(1,6)
+print("WELCOME TO THE DICE GAME!!!!!!!!")
+print("only use yes")
+print("Your number is...")
+print(dice)
+roll_again = input("Do you want to roll again? ")
+
+while roll_again == "yes":
+    print("Your number is...")
+    print(random.randint(1,6))
+    roll_again = input("Do you want to roll again? ")
+    if roll_again == "yes":
+        continue
+    else:
+        print("BYE!!!!!")
+        exit()
+    
+    
+    
+
 
